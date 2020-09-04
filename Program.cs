a4f504eb283837a3148dc2b9792ca8392cfdc6d3
irockmywarlock <noemirsevilla@gmail.com>
2020-09-03 21:53:28 -0500

    first init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hellonetcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
